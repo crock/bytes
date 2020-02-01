a0d5890f97d312311fa03a5058fd306e6de51da5
Alex Crocker <hello@crock.dev>
2020-02-01 21:43:47 +0000

    Changed the order of posts to descending

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,7 +7,7 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
   const result = await graphql(`
     {
       allMarkdownRemark(
-        sort: { order: ASC, fields: [frontmatter___date] }
+        sort: { order: DESC, fields: [frontmatter___date] }
         limit: 1000
       ) {
         edges {
